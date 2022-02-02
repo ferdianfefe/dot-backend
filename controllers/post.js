0e05251ce782153a472a53260e6308bf80480efc
LatianOTI <ccobaabocc00@gmail.com>
2022-02-02 14:32:25 +0700

    m

--- controllers/post.js
+++ controllers/post.js
@@ -1,15 +1,5 @@
 'use strict'
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-// function createPost(req, res){
-//     let {text, image, userId} = req.body; }
-// }
-=======
 function createPost(req, res){
-    let {text, image, userId} = req.body; }
+    let {text, image, userId} = req.body;
 }
->>>>>>> 8c7b042cc4cf016d4afcc812d34f214def4d4860
-=======
-halo
->>>>>>> d60174854ce1429ce9a92354dd2e728a8df9ba93
+halo fer
